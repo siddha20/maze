d1ea84930bf59b0c129b5c562400d72642497dac
sid kil <sidkil@sids-MacBook-Pro-4.local>
2019-09-11 21:45:23 -0600

    game is now working wihtout graphics

--- main.py
+++ main.py
@@ -8,14 +8,52 @@ np.random.seed(238)
 random.seed(299)
 
 
+class Game: 
+	def __init__(self, grid, player):
+		self.grid = grid
+		self.player = player
+		self.position = np.zeros([2])
+		self.position[0] = int(self.grid.size/2)
+		self.position[1] = int(self.grid.size/2)
+		self.player.update_position(self.position)
+		self.state = self.grid.map[int(self.position[0]), int(self.position[1])]
+		self.reward = None
+		self.done = False
+	def re_init(self):
+		self.done = False
+		self.position[0] = int(self.grid.size/2)
+		self.position[1] = int(self.grid.size/2)
+		self.reward = None
+		self.state =  self.grid.map[int(self.position[0]), int(self.position[1])]
+		return self.state
+	def action_state(self, action):
+		self.player.update_position(self.player.do_action(action))
+		self.update_state()
+		self.update_reward()
+	def update_reward(self):
+		if self.state == 1:
+			self.reward = -100
+			self.game_over()
+		if self.state == 2:
+			self.reward = 100
+			self.game_over()
+		if self.state == 0:
+			self.reward = 10
+	def update_state(self):
+		self.state = self.grid.map[int(self.player.position[0]), int(self.player.position[1])]
+	def game_over(self):
+		self.done = True
+	def get_info(self):
+		return (self.state, self.reward, self.done, self.grid.model_player(self.position))
+
 class Grid:
-	def __init__(self, trap_num, size):
+	def __init__(self, max_trap_per_row, size):
 		self.map = np.zeros([size,size]) #size will be 8
 		self.normal_square = 0
 		self.bad_square = 1
 		self.goal_square = 2
-		self.trap_num = trap_num
 		self.size = size
+		self.max_trap_per_row = max_trap_per_row
 
 	def makeGrid(self):
 		trap_x_count = 0
@@ -25,24 +63,23 @@ class Grid:
 				trap_x_count = 0
 				for x in range(0,self.map.shape[1]):
 					rand = random.randint(0,100)
-					if rand < 30 and self.map[y,x] == 0 and trap_x_count < 2:
+					if rand < 30 and self.map[y,x] == 0 and trap_x_count < self.max_trap_per_row:
 						self.map[y,x] = self.bad_square
 						trap_x_count += 1
 					if rand > 90 and self.map[y,x] == 0 and goal_count == 0 and y > 3:
 						self.map[y,x] = self.goal_square
 						goal_count +=1
+	def model_player(self, position):
+		map_copy = np.copy(self.map)
+		map_copy[int(position[0]), int(position[1])] = 99
+		return map_copy
 
 class Player:
-	def __init__(self, grid):
-		self.grid = grid
-		self.position = np.zeros([2])
+	def __init__(self):
+		self.position = None
 		self.state = None
-		self.latest_reward = None
-		self.position[0] = int(self.grid.size/2)
-		self.position[1] = int(self.grid.size/2)
-		self.done = False
-		self.update_state()
-
+	def update_position(self, position):
+		self.position = position
 	def do_action(self, action): # up down left right 0 1 2 3
 		if action == 0: 
 			self.position[0] -= 1
@@ -51,47 +88,42 @@ class Player:
 		if action == 2: 
 			self.position[1] -= 1
 		if action == 3:
-			self.position[1] += 1	
-		self.latest_reward = self.update_state()
+			self.position[1] += 1
+		return self.position	
+	def get_position(self):
+		return self.position
 
-	def update_state(self):
-		self.state = grid.map[int(self.position[0]), int(self.position[1])]
-		reward = 0
-		if self.state == 1:
- 			reward = -100
- 			self.done = True
- 			self.reset()
-		if self.state == 2:
-			reward = 100
-			self.done = True
-			self.reset()
-		if self.state == 0:
-			reward = 10
-		return reward
-
-	def reset(self):
-		self.position[0] = int(self.grid.size/2)
-		self.position[1] = int(self.grid.size/2)
-		self.done = False 
-
-	def get_info(self):
-		return(self.state, self.latest_reward, self.position, self.done)		
 
 if __name__ == "__main__":
-	grid = Grid(18,9)
+	grid = Grid(0, 9)
 	grid.makeGrid()
 	print(grid.map)
-	player = Player(grid)
-	player.reset()
-	count = 0
-	while True:
-		print(count)
-		player.do_action(3)
-		state, reward, position, done = player.get_info()
-		print(state, reward, position, done)
-		if player.done:
-			break
-		count += 1
+	player = Player()
+	game = Game(grid, player)
+	for i in range(0,1):
+		game.re_init()
+		while True:
+			if game.done:
+				print('game done')
+				break
+			action = random.randint(0,3)
+			if action == 0:
+				print('up')
+			if action == 1:
+				print('down')
+			if action == 2:
+				print('left')
+			if action == 3:
+				print('right')
+			game.action_state(random)
+			state, reward, done, player_position = game.get_info()
+			print(player_position)
+			print(reward)
+
+
+
+
+	
 
 	
 
